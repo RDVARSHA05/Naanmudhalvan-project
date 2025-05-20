22e62a8d2a1e2a4782d375f175f9679f8cc233d2
RDVARSHA05 <23l208@psgitech.ac.in>
2025-05-20 22:12:12 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,5 +3,5 @@ from src.preprocessing import load_and_preprocess_data
 from src.model import train_and_save_model
 
 if __name__ == "__main__":
-    X, y = load_and_preprocess_data("data/heart_disease.csv")
+    X, y = load_and_preprocess_data("synthetic_wearable_health_data.csv")
     train_and_save_model(X, y)
